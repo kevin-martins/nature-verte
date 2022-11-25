bae44eb1c2b786511424dc70735aef60922bfb82
Kevin <kev.martinsvieira@gmail.com>
2022-11-25 14:43:33 +0100

    feat: init backend and move all front to frontend folder

---
+++ backend/app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user');
+const plantRoutes = require('./routes/plants');
+
+mongoose.connect()
